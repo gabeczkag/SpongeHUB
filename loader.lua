11cc4c690803d7a178f9d03af35290385b25fcaf
GabeczkaG <karczmarczykgrzegorz100@gmail.com>
2026-09-06 20:51:00 +0200

    Print actual GameId when invalid

--- loader.lua
+++ loader.lua
@@ -5,4 +5,5 @@ if game.GameId == mm2 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/gabeczkag/SpongeHUB/main/scripts/mm2/Script.luau"))()
 	else
 	print("The GameID in Invalid")
+	print(game.GameId)
 end

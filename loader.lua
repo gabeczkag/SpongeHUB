0603bb7bfe04ab9cc77073aa3a6d3953740e96f4
GabeczkaG <karczmarczykgrzegorz100@gmail.com>
2026-09-06 20:46:08 +0200

    Add invalid GameID message to loader

--- loader.lua
+++ loader.lua
@@ -3,4 +3,6 @@ local mm2 = 142823291
 
 if game.GameId == mm2 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/gabeczkag/SpongeHUB/main/scripts/mm2/Script.luau"))()
+	else
+	print("The GameID in Invalid")
 end

f21a18e438e9b08f83248919cfc05fd1dfa5930f
GabeczkaG <karczmarczykgrzegorz100@gmail.com>
2026-09-06 20:43:54 +0200

    Fix loader URL to point to raw script

--- loader.lua
+++ loader.lua
@@ -2,5 +2,5 @@ local mm2 = 142823291
 
 
 if game.GameId == mm2 then
-	loadstring(game:HttpGet("https://link-do-twojego-skryptu.com"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gabeczkag/SpongeHUB/main/scripts/mm2/Script.luau"))()
 end
